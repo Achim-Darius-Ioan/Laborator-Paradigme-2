b8824bdd26bfad942d2d75202344620fe9249c24
Achim-Darius-Ioan <dariusioan.achim@ulbsibiu.ro>
2025-03-18 08:33:15 +0200

    Achim Darius-Ioan Laborator 2

--- src/ro/ulbs/paradigme/lab2/Form.java
+++ src/ro/ulbs/paradigme/lab2/Form.java
@@ -2,16 +2,26 @@ package ro.ulbs.paradigme.lab2;
 
 public class Form {
     private String color;
+    private static int counter = 0;
 
     public Form() {
         this.color = "white";
+        counter++;
     }
+
     public Form(String color) {
         this.color = color;
+        counter++;
     }
+
     public float getArea() {
         return 0;
     }
+
+    public static int getCounter() {
+        return counter;
+    }
+
     @Override
     public String toString() {
         return "This form has the color " + color;
